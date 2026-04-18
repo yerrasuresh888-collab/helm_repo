ef76ab5a84915f9058f958fdfd6e28b6f965401f
Suresh <yerrasuresh888@gmail.com>
2026-04-18 12:45:32 +0530

    added var log_level

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -10,7 +10,8 @@ fi
 echo "Deploying to $ENV..."
 
 helm upgrade --install my-app ./chart \
-  -f configs/deploy.hel.charts.yml \
+  -f configs/deploy.helm.charts.yml \
   --set environment=$ENV
+  --set backend.configMaps.LOG_LEVEL="${LOG_LEVEL}"
 
 echo "Done!"
